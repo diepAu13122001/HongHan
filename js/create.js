e8e7ba161f07099a00a67906168f0a83273c183a
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-06-18 19:50:40 +0700

    update

--- js/create.js
+++ js/create.js
@@ -6,47 +6,48 @@ const tittle = document.getElementById("Tittle").value;
 const type = document.getElementById("Type").value;
 const amount = document.getElementById("Amount").value;
 const createdBy = document.getElementById("createdBy").value;
-const createdAt = document.getElementById("createdBy").value;
+const createdAt = document.getElementById("date").value;
 
 const btnCreate = document.getElementById("create");
 
 const create = async (db) => {
-    // kiểm tra nếu có trơờng trống thì phải báo lỗi 
-    if(tittle == "" || tittle == undefined) {
-        alert("Please fill form");
-        return; // thoat ra khoi function de khong lam tiep code
-    }
-    if(type == "" || type == undefined) {
-        alert("Please fill form");
-        return; // thoat ra khoi function de khong lam tiep code
-    }
-    if(amount == "" || amount == undefined) {
-        alert("Please fill form");
-        return; // thoat ra khoi function de khong lam tiep code
-    }
-    if(createdBy == "" || createdBy == undefined) {
-        alert("Please fill form");
-        return; // thoat ra khoi function de khong lam tiep code
-    }
-    if(createdAt == "" || createdAt == undefined) {
-        alert("Please fill form");
-        return; // thoat ra khoi function de khong lam tiep code
-    }
+    // // kiểm tra nếu có trơờng trống thì phải báo lỗi 
+    // if (tittle == "" || tittle == undefined) {
+    //     alert("Please fill form");
+    //     return; // thoat ra khoi function de khong lam tiep code
+    // }
+    // if (type == "" || type == undefined) {
+    //     alert("Please fill form");
+    //     return; // thoat ra khoi function de khong lam tiep code
+    // }
+    // if (amount == "" || amount == undefined) {
+    //     alert("Please fill form");
+    //     return; // thoat ra khoi function de khong lam tiep code
+    // }
+    // if (createdBy == "" || createdBy == undefined) {
+    //     alert("Please fill form");
+    //     return; // thoat ra khoi function de khong lam tiep code
+    // }
+    // if (createdAt == "" || createdAt == undefined) {
+    //     alert("Please fill form");
+    //     return; // thoat ra khoi function de khong lam tiep code
+    // }
 
     //tạo dữ liệu
     try {
         const docRef = await addDoc(collection(db, "tasts"), {
-          Tittle: "di an",
-          Amount: 200000,
-          Type: "Outcome",
-          createdAt: Date.now()  ,
-          createdBy:"Han"
+            Tittle: tittle,
+            Amount: amount,
+            Type: type,
+            createdAt: createdAt,
+            createdBy: createdBy
         });
-      
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
+
+        alert("Created data");
+        console.log(docRef.id)
+    } catch (e) {
         console.error("Error adding document: ", e);
-      }
+    }
 
 }
 
--- js/table.js
+++ js/table.js
@@ -1,34 +1,63 @@
- // Import the functions you need from the SDKs you need
- import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-lite.js";
+// Import the functions you need from the SDKs you need
+import { collection, getDocs, addDoc, deleteDoc, doc, query, where } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-lite.js";
 import { db } from "./initApp.js";
 
-const getData = async (db) => {
-    const querySnapshot = await getDocs(collection(db, "tastes"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
+// khai bao du lieu
+const income = document.getElementById("income-data");
+const outcome = document.getElementById("outcome-data");
+const saving = document.getElementById("saving-data");
+
+
+
+// lay du lieu va loc du lieu theo loai
+const getData = async (db, type) => {
+  // loc theo type
+  const q = query(collection(db, "tasts"), where("Type", "==", type));
+  console.log(await getDocs(collection(db, "tasts")));
+  const querySnapshot = await getDocs(q);
+  let rows = ``;
+  querySnapshot.forEach((doc) => {
+    rows += `<tr>
+      <th scope="row">${doc.data().createdAt}</th>
+      <td>${doc.data().createdBy}</td>
+      <td>${doc.data().Tittle}</td>
+      <td>${doc.data().Amount}</td>
+      <td>${doc.data().Type}</td>
+
+    </tr>`
+  });
+  return rows;
+}
+// neu co thi hien thi du lieu vua load duoc ra man hinh
+if(income)income.innerHTML = await getData(db, "Income");
+if(outcome) outcome.innerHTML = await getData(db, "Outcome");
+if(saving) saving.innerHTML = await getData(db, "Saving");
+
+
+//delete data from collection
+const removeItem = async (id) => {
+  await deleteDoc(doc(db, "tasts", id));
+  location.reload();
+}
+
+
+//create data for a collection
+const createData = async (db, object) => {
+  try {
+    const docRef = await addDoc(collection(db, "tasts"), {
+      Tittle: "di an",
+      Amount: 200000,
+      Type: "Outcome",
+      createdAt: "12/12/2001",
+      createdBy: "Han"
     });
-    return querySnapshot.docs;
- }
 
- //delete data from collection
- const deleteDataById = async (db, id) => {
-    await deleteDoc(doc(db,"tastes", id));
- }
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
 
- //create data for a collection
- const createData = async (db,object) => {
-    try {
-        const docRef = await addDoc(collection(db, "tasts"), {
-          Tittle: "di an",
-          Amount: 200000,
-          Type: "Outcome",
-          createdAt: Date.now()  ,
-          createdBy:"Han"
-        });
-      
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
- }
+
+
